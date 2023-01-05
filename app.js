a50dbc5c4a834a0e5e583e4e15e26ab57a417621
Katherine <katherineleefoley@gmail.com>
2023-01-05 17:37:55 -0600

    added two more arrows

--- app.js
+++ app.js
@@ -119,16 +119,24 @@ let sum = (a, b, c, d) => (a + b + c + d);
 console.log(sum(1, 2, 3, 4));
 
 
-let objectLit = function() {
-  return {
+// let objectLit = function() {
+//   return {
+//     key1: 'value1',
+//     key2: 'value2',
+//     key3: 'value3',
+//   };
+// };
+
+let objectLit = () => {
+    return {
     key1: 'value1',
     key2: 'value2',
     key3: 'value3',
-  };
+    };
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(objectLit());
+console.log(objectLit());
 
 
 let sumAndProduct = function(a, b) {
@@ -141,12 +149,15 @@ let sumAndProduct = function(a, b) {
 // console.log(sumAndProduct(3, 9));
 
 
-let message = function(name) {
-  return `Hello, ${name}!`;
-};
+// let message = function(name) {
+//   return `Hello, ${name}!`;
+// };
 
+let message = (name) => {
+    return `Hello, ${name}!`;
+};
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(message('Allie'));
+console.log(message('Allie'));
 
 
 let Student = function(name, age, hometown) {
@@ -159,7 +170,7 @@ let joe = new Student('Joe Schmoe', 100, 'Anytown, USA');
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this code to break!
-// console.log(joe);
+console.log(joe);
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
@@ -206,4 +217,4 @@ Student.prototype.scopeArrow = () => console.log(this);
 // 2. What is "this" when joe.scopeArrow() is invoked?
 //
 // 3. Explain why "this" is different when an arrow function is used.
-//
+//vbg
